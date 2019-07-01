67cb59dd1bc5d5a8008edeb91053fa42f90b0615
rkaushik <rkaushik@tavisca.in>
2019-07-01 12:41:18 +0530

    Made the code modular

---
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/HelperClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
+{
+    class HelperClass
+    {
+        public static int RequiredDigit(int num, int i)
+        {
+            int Reverse = 0;
+            while (num > 0)
+            {
+                int remainder = num % 10;
+                Reverse = (Reverse * 10) + remainder;
+                num = num / 10;
+            }
+            int j = 0;
+            while (Reverse > 0)
+            {
+                if (i == j)
+                {
+                    return Reverse % 10;
+                }
+                else
+                {
+                    Reverse /= 10;
+                    j++;
+                }
+            }
+            return -1;
+        }
+
+        public static bool ispossible(string str, int num)
+        {
+            var charsToRemove = new string[] { "?" };
+            foreach (var c in charsToRemove)
+            {
+                str = str.Replace(c, string.Empty);
+            }
+            int compare = System.Convert.ToInt32(str);
+            if (compare >= num) return false;
+            return true;
+        }
+        public static string[] SplitString(string equation)
+        {
+            string s1 = "", s2 = "", s3 = "", temp = "";
+            for (int i = 0; i < equation.Length; i++)
+            {
+                if (equation[i] == '*')
+                {
+                    s1 = temp;
+                    temp = "";
+                }
+                else if (equation[i] == '=')
+                {
+                    s2 = temp;
+                    temp = "";
+                }
+                else if (i == equation.Length - 1)
+                {
+                    temp = temp + equation[i];
+                    s3 = temp;
+                }
+                else temp = temp + equation[i];
+            }
+            string[] ans = { s1, s2, s3 };
+            return ans;
+        }
+    }
+}
--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -19,64 +19,14 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"{args} : {result}");
         }
-        public static int RequiredDigit(int num,int i)
-        {
-           int Reverse = 0;  
-            while(num>0)  
-            {  
-                int remainder = num % 10;  
-                Reverse = (Reverse * 10) + remainder;  
-                num = num / 10;  
-            }  
-            int j=0;
-            while(Reverse>0)
-            {
-                if(i==j)
-                {
-                    return Reverse%10;
-                }
-                else
-                {
-                    Reverse/=10;
-                    j++;
-                }
-            }
-            return -1;
-        }
-        public static bool ispossible(string str,int num)
-        {
-            var charsToRemove = new string[] { "?"};
-            foreach (var c in charsToRemove)
-            {
-               str = str.Replace(c, string.Empty);
-            }   
-            int compare=System.Convert.ToInt32(str);
-            if(compare>=num) return false;
-            return true;
-        }
+
+
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            string s1="",s2="",s3="",temp="";
-              for(int i=0;i<equation.Length;i++)
-              {
-    	        if(equation[i]=='*')
-    	        {
-    		        s1=temp;
-    		        temp="";
-    	        }
-    	        else if(equation[i]=='=')
-    	        {
-    		        s2=temp;
-    		        temp="";
-    	        }
-    	        else if(i==equation.Length-1)
-    	        {
-    		        temp=temp+equation[i];
-    		        s3=temp;
-    	        }
-    	        else temp=temp+equation[i];	
-              }
+            // Split the string
+            string s1="",s2="",s3="";
+            string[] Split = HelperClass.SplitString(equation);
+            s1 = Split[0];s2 = Split[1];s3 = Split[2];
               int num1=0,num2=0,num3=0;
               if (s1.Contains("?"))
               {
@@ -89,7 +39,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 num1=num3/num2;
                 //Now check if it is impossible to place any digit in place of ?
                 string str=s1;
-                if(!ispossible(str,num1)) return -1;
+                if(!HelperClass.ispossible(str,num1)) return -1;
                 int i=0;
                 for(i=0;i<s1.Length;i++)
                 {
@@ -99,9 +49,10 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                   }
                 }
                 //Now we need to return the digit present at ith position in num1
-                 int ans=RequiredDigit(num1,i);
+                 int ans=HelperClass.RequiredDigit(num1,i);
                  return ans;
               }
+
               else if(s2.Contains("?"))
               {
                   num3=System.Convert.ToInt32(s3);
@@ -113,7 +64,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                   num2=num3/num1;
                   //Now check if it is impossible to place any digit in place of ?
                   string str=s2;
-                  if(!ispossible(str,num2)) return -1;
+                  if(!HelperClass.ispossible(str,num2)) return -1;
                   int i=0;
                   for(i=0;i<s2.Length;i++)
                   {
@@ -122,9 +73,10 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                      break;
                     }
                   }
-                  int ans=RequiredDigit(num2,i);
+                  int ans=HelperClass.RequiredDigit(num2,i);
                   return ans;
               }
+
               else
               {
                   num1=System.Convert.ToInt32(s1);
@@ -132,7 +84,7 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                   num3=num1*num2;
                   //Now check if it is impossible to place any digit in place of ?
                   string str=s3;
-                  if(!ispossible(str,num3)) return -1;
+                  if(!HelperClass.ispossible(str,num3)) return -1;
                   int i=0;
                   for(i=0;i<s3.Length;i++)
                   {
@@ -141,10 +93,11 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                      break;
                     }
                   }
-                  int ans=RequiredDigit(num3,i);
+                  int ans=HelperClass.RequiredDigit(num3,i);
                   return ans;
-              }              
+              }      
             throw new NotImplementedException();
         }
+
     }
 }
